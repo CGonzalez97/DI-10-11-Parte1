56082374eb3850894f642b21da6fb2233555d12a
CGonzalez97 <cgl76490@gmail.com>
2020-11-10 10:16:47 +0100

    Practica2

---
+++ segundaPracticaAngular/src/app/Componentes/lista-correos/lista-correos.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-correos',
+  templateUrl: './lista-correos.component.html',
+  styleUrls: ['./lista-correos.component.scss']
+})
+export class ListaCorreosComponent implements OnInit {
+
+  correos:any[];
+
+  constructor() { 
+
+    const correo1 ={
+      titulo: 'Titulo1',
+      cuerpo: 'curpo1curpo1curpo1curpo1curpo1',
+      emisor: 'emisor1@SpeechGrammarList.com',
+      destinatario: 'destinatario1@gmail.com',
+      leido: false
+    }
+    const correo2 ={
+      titulo: 'Titulo2',
+      cuerpo: 'curpo2curpo2curpo2curpo2curpo2',
+      emisor: 'emisor2@SpeechGrammarList.com',
+      destinatario: 'destinatario2@gmail.com',
+      leido: true
+    }
+    this.correos = [];
+    this.correos.push(correo1);
+    this.correos.push(correo2);
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
